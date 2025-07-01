bb8bb59c06529e2cbfb7f6c93e1613f00f8b95cb
gabrielariver <riv23011@byui.edu>
2025-07-01 18:02:25 -0500

    Update part 1 - Project

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const mongodb = require('./data/contacts'); 
 const app = express();
-const port = process.env.PORT || 8080;
+
+const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        app.listen(port, () => {console.log(`The database is running on port ${port}`)});
+    }
+}); 
